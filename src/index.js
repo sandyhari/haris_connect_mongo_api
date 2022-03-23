5dccee5793051ee8846ccda01285726d3bb8426d
srihari <sandyhari333@gmail.com>
2022-03-23 19:53:26 +0530

    removed unsupported client options

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ async function connectDB(dbConfig, dbName = 'label') {
     try {
         const mongoOptions = Object.assign({}, dbConfig.options, {
             useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true,
-            useFindAndModify: false
+            useUnifiedTopology: true
         })
         console.log(`Attempting to establish a connection to ${dbName} mongo db...`);
         const client = await MongoClient.connect(dbConfig.connectionURL, mongoOptions);
